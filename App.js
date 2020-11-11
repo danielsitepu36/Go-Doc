e644dd6f2571f9ca97f80ee5e98777394fcda454
RianWardanaPutra <f.rianwardanaputra@gmail.com>
2020-11-12 00:14:21 +0700

    working login system

--- App.js
+++ App.js
@@ -11,41 +11,42 @@ import HomeAdmin from './components/admin/homeAdmin';
 import HomeDokter from './components/dokter/homeDokter';
 import HomePasien from './components/pasien/homePasien';
 import Router from './components/begin/router';
+import * as RouteNavigator from './components/RootNavigation';
+import Login from './components/begin/login';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+// TODO: implement login
+// Jadi flownya gini
+// 1) Open app, cek localStorage, kalo ada dia ga lewat login
+// 2) Kalo belom login, ke login (Ini 1 2 ditaruh di App / splash)
+// 3) Habis login, datanya dimasukin firebase & local (ini di login)
+// dah
+// Prioritas: Firestore akses & login
+// Kalau mau sebenernya bikin screen menu dulu aja.. passing user bisa blakang
+// Jadi yg penting kita bisa liat list kategori & jadwal dokter
+// User kosongan gapap
+// At least ada fitur useful disini yg bisa dikumpul
 
 const Stack = createStackNavigator();
 // const Tab = createBottomTabNavigator();
 
-class App extends Component {
+export default class App extends Component {
+  state = {
+    userinf: null,
+  };
   constructor(props) {
     super(props);
-
     this.state = {isLoading: true};
   }
 
-  performTimeConsumingTask = async () => {
-    return new Promise((resolve) =>
-      setTimeout(() => {
-        resolve('result');
-      }, 2000),
-    );
-  };
-
-  async componentDidMount() {
-    // Preload data from an external API
-    // Preload data using AsyncStorage
-    const data = await this.performTimeConsumingTask();
-
-    if (data !== null) {
-      this.setState({isLoading: false});
-    }
-  }
-
   render() {
-    if (this.state.isLoading) {
-      return <SplashScreen />;
-    }
+    // if (this.state.isLoading) {
+    //   return <SplashScreen />;
+    // }
+    // if (this.data !== null) {
+    // this.props.navigation.navigate('Home');
+    // // <Router />
     return (
-      // <Router />
       <NavigationContainer>
         <Stack.Navigator
           initialRouteName="HomePasien"
@@ -56,12 +57,12 @@ class App extends Component {
             headerTintColor: '#fff',
           }}>
           {/* <Stack.Screen
-            name="SplashScreen"
-            component={SplashScreen}
-            options={{
-              headerShown: false,
-            }}
-          /> */}
+              name="SplashScreen"
+              component={SplashScreen}
+              options={{
+                headerShown: false,
+              }}
+            /> */}
           <Stack.Screen
             name="Home"
             component={Home}
@@ -71,9 +72,9 @@ class App extends Component {
           />
           <Stack.Screen
             name="HomePasien"
-            component={HomePasien}
+            component={Login}
             options={{
-              title: 'Home',
+              title: 'Login',
             }}
           />
           <Stack.Screen
@@ -95,5 +96,18 @@ class App extends Component {
     );
   }
 }
+// }
 
-export default App;
+// const AppContainer = createAppContainer(InitialNavigator);
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <Provider>
+//         <AppContainer />
+//       </Provider>
+//     );
+//   }
+// }
+
+// export default App;
--- components/begin/login.js
+++ components/begin/login.js
@@ -1,149 +1,150 @@
-// import {
-//   GoogleSignin,
-//   GoogleSigninButton,
-// } from '@react-native-community/google-signin';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
+// import auth from '@react-native-firebase/auth';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from '@react-native-community/google-signin';
+// import firestore from '@react-native-firebase/firestore';
 import React, {Component, useState} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-// import Art from '../../resource/art1.svg';
-
-// import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Button} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+// import AsyncStorage from '@react-native-community/async-storage';
 // import {Button} from 'react-native-elements';
+// import Icon from 'react-native-vector-icons/FontAwesome';
 // import Home from './home';
+import {loadUser, saveUser, clearUser} from '../util/userStorage';
+import Art from '../../resource/art1.svg';
 
-// GoogleSignin.configure({
-//   // webClientId:'143097564914-u59plvqp16dse79tcrqdqoiqovnied2i.apps.googleusercontent.com',
-// });
+GoogleSignin.configure({
+  webClientId:
+    '1015789846201-ouivu783us3flbfc9rpiplf6ot0mi2oq.apps.googleusercontent.com',
+  // offlineAccess: true,
+  // scopes: 'https://www.googleapis.com/auth/admin.directory.user.readonly',
+});
+// async function onGoogleButtonPress() {
+//   // Get the users ID token
+//   const {idToken} = await GoogleSignin.signIn();
 
-export default class Login extends Component {
+//   // Create a Google credential with the token
+//   const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+//   // Sign-in the user with the credential
+//   return auth().signInWithCredential(googleCredential);
+// }
+
+class GoogleSignIn extends Component {
+  //   this.state = {
+  //     userinf: null,
+  //   };
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+    };
+  }
+  // const [user, setUser] = useState(null);
+
+  getData = async () => {
+    try {
+      const jsonValue = await loadUser();
+      console.log('GET DATA:' + JSON.stringify(jsonValue));
+      return jsonValue != null ? jsonValue : null;
+    } catch (e) {
+      // error reading value
+    }
+  };
+
+  signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      this.setState({
+        user: userInfo,
+      });
+      saveUser(this.state.user);
+      console.log('SIGNED' + JSON.stringify(this.state.user));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  getLocalData = async () => {
+    try {
+      const localData = await this.getData();
+      // console.log('MOUNT:', localData);
+      // if (localData != null) {
+      //   this.setState({
+      //     user: localData,
+      //   });
+      // } else {
+      //   this.setState({
+      //     user: null,
+      //   });
+      // }
+      // await saveUser(localData);
+      console.log('localdata:' + JSON.stringify(localData));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  UNSAFE_componentWillMount() {
+    this.getLocalData();
+  }
+
+  signOut = async () => {
+    console.log('SignOut');
+    try {
+      await GoogleSignin.revokeAccess();
+      await GoogleSignin.signOut();
+      // await AsyncStorage.clear();
+      await clearUser();
+      this.setState({
+        user: null,
+      }); // Remember to remove the user from your app's state as well
+    } catch (error) {
+      console.log(error);
+    }
+  };
   render() {
     return (
-      <View>
-        <Text>Selamat Datang di Go-Doc</Text>
-        {/* <Art /> */}
-        {/* <GoogleSigninButton /> */}
+      <View style={styles.body}>
+        <Text>Login</Text>
+        <GoogleSigninButton
+          style={{width: 200, height: 50, marginBottom: 20}}
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Light}
+          onPress={() => this.signIn()}
+        />
+        {console.log('USER:', this.state.user)}
+        <Text>
+          {this.state.user != null
+            ? this.state.user.user.name
+            : 'Please Sign In'}
+        </Text>
+        <Button
+          title="SignOut"
+          style={{
+            width: 200,
+            marginTop: 10,
+          }}
+          onPress={() => this.signOut()}
+        />
+        {/* <Text>{getData() != null ? getData() : 'No Account'}</Text> */}
+        <Button
+          title="Load"
+          style={{width: 200, marginTop: 10}}
+          onPress={() => this.getLocalData()}
+        />
       </View>
     );
   }
 }
 
-// class Login extends Component {
-//   //   this.state = {
-//   //     userinf: null,
-//   //   };
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       user: null,
-//     };
-//   }
-//   // const [user, setUser] = useState(null);
+export default GoogleSignIn;
 
-//   storeData = async (value) => {
-//     try {
-//       const jsonValue = JSON.stringify(value);
-//       await AsyncStorage.setItem('user', jsonValue);
-//     } catch (e) {
-//       console.log(e);
-//     }
-//   };
-
-//   getData = async () => {
-//     try {
-//       const jsonValue = await AsyncStorage.getItem('user');
-//       console.log('GET DATA:' + jsonValue);
-//       return jsonValue != null ? JSON.parse(jsonValue) : null;
-//     } catch (e) {
-//       // error reading value
-//     }
-//   };
-
-//   signIn = async () => {
-//     try {
-//       await GoogleSignin.hasPlayServices();
-//       const userInfo = await GoogleSignin.signIn();
-//       this.setState({
-//         user: userInfo,
-//       });
-//       await this.storeData(this.state.user);
-//       console.log('SIGNED' + this.state.user);
-//       return <Home user={user} />;
-//     } catch (e) {
-//       console.log(e);
-//     }
-//   };
-
-//   getLocalData = async () => {
-//     try {
-//       const localData = await this.getData();
-//       console.log('MOUNT:', localData);
-//       if (localData != null) {
-//         this.setState({
-//           user: localData,
-//         });
-//       } else {
-//         this.setState({
-//           user: null,
-//         });
-//       }
-//     } catch (e) {
-//       console.log(e);
-//     }
-//   };
-
-//   UNSAFE_componentWillMount() {
-//     this.getLocalData();
-//   }
-
-//   signOut = async () => {
-//     console.log('SignOut');
-//     try {
-//       await GoogleSignin.revokeAccess();
-//       await GoogleSignin.signOut();
-//       await AsyncStorage.clear();
-//       this.setState({
-//         user: null,
-//       }); // Remember to remove the user from your app's state as well
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-//   render() {
-//     return (
-//       <View style={styles.body}>
-//         <Text>Login</Text>
-//         <GoogleSigninButton
-//           style={{width: 200, height: 50, marginBottom: 20}}
-//           size={GoogleSigninButton.Size.Wide}
-//           color={GoogleSigninButton.Color.Light}
-//           onPress={() => this.signIn()}
-//         />
-//         {console.log('USER:', this.state.user)}
-//         <Text>
-//           {this.state.user != null
-//             ? this.state.user.user.name
-//             : 'Please Sign In'}
-//         </Text>
-//         <Button
-//           title="SignOut"
-//           buttonStyle={{
-//             width: 200,
-//             marginTop: 10,
-//           }}
-//           onPress={() => this.signOut()}
-//         />
-//         {/* <Text>{getData() != null ? getData() : 'No Account'}</Text> */}
-//         <Button
-//           title="Load"
-//           buttonStyle={{
-//             width: 200,
-//             marginTop: 10,
-//           }}
-//           onPress={() => this.getLocalData()}
-//         />
-//       </View>
-//     );
-//   }
-// }
-
-// export default Login;
+const styles = StyleSheet.create({
+  body: {
+    marginLeft: '25%',
+    marginTop: 40,
+  },
+});
