d6f92eb140ecf3654bcb74920f7c58dc78b1962d
danielsitepu36 <danielsitepu96@gmail.com>
2020-11-22 01:00:04 +0700

    Home & Login UI fix, fitur Cek & BuatRekamMedis

--- web_go-doc/src/components/layout/HomeNavbar.js
+++ web_go-doc/src/components/layout/HomeNavbar.js
@@ -9,26 +9,54 @@ import Button from "@material-ui/core/Button";
 
 // Icons
 import HomeIcon from "@material-ui/icons/Home";
-import GodocLogo from "../../assets/GodocLogo.png";
+import GodocWhiteLogo from "../../assets/GodocWhiteLogo.png";
+import Heart from "../../assets/Heart.png";
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  Typography,
+} from "@material-ui/core";
 
 class Navbar extends Component {
   render() {
     return (
-      <AppBar style={{ background: "#e00000" }}>
-        <Toolbar>
-          <Link to="/">
-            <img src={GodocLogo} width="150px" />
-          </Link>
-          <div style={{ marginLeft: "auto" }}>
-            <Button color="inherit" component={Link} to="/">
-              Home
-            </Button>
-            <Button color="inherit" component={Link} to="/dashboard">
-              Dashboard
-            </Button>
-          </div>
-        </Toolbar>
-      </AppBar>
+      <>
+        <AppBar style={{ background: "#e00000" }}>
+          <Toolbar>
+            <Link to="/">
+              <img src={GodocWhiteLogo} width="150px" />
+            </Link>
+            <div style={{ marginLeft: "auto" }}>
+              <Button color="inherit" component={Link} to="/">
+                Home
+              </Button>
+              <Button color="inherit" component={Link} to="/dashboard">
+                Dashboard
+              </Button>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <div
+          style={{
+            backgroundColor: "#e00000",
+            overflow: "hidden",
+            position: "fixed",
+            bottom: "0",
+            width: "101%",
+            marginLeft: "-1%",
+            height: "40px",
+            paddingRight: "1%",
+            paddingTop: "15px",
+            textAlign: "center",
+          }}
+        >
+          <Typography variant="subtitle1" style={{ color: "white" }}>
+            Made with
+            <img src={Heart} width="20px" style={{ margin: "2px 8px" }} />
+            by Tepu, Rian, Adhit
+          </Typography>
+        </div>
+      </>
     );
   }
 }
--- web_go-doc/src/pages/Login.js
+++ web_go-doc/src/pages/Login.js
@@ -15,26 +15,83 @@ import {
 } from "@react-firebase/firestore";
 import { firebaseConfig, auth, db } from "../util/config";
 import HomeNavbar from "../components/layout/HomeNavbar";
-import { Button } from "@material-ui/core";
+import { Button, Grid, Typography } from "@material-ui/core";
+
+import GodocArt from "../assets/GodocArt2.png";
+import GodocLogo from "../assets/GodocLogo.png";
 
 class Login extends Component {
   render() {
     return (
       <div>
         <HomeNavbar />
-        <Button
-          style={{
-            backgroundColor: "#e00000",
-            color: "#fff",
-            marginTop: "10px",
-          }}
-          onClick={async () => {
-            const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-            await firebase.auth().signInWithPopup(googleAuthProvider);
-          }}
-        >
-          Sign In with Google
-        </Button>
+        <Grid container style={{ padding: "100px" }}>
+          <Grid
+            lg={6}
+            md={12}
+            sm={12}
+            style={{ textAlign: "center", margin: "auto" }}
+          >
+            <div
+              style={{
+                textAlign: "left",
+                marginLeft: "50px",
+              }}
+            >
+              <Typography
+                variant="h4"
+                align="center"
+                style={{
+                  color: "#444444",
+                  textAlign: "left",
+                  fontWeight: "bold",
+                }}
+              >
+                Anda belum masuk,
+              </Typography>
+              <Typography
+                variant="h4"
+                align="center"
+                style={{
+                  color: "#444444",
+                  textAlign: "left",
+                }}
+              >
+                Silakan masuk untuk melanjutkan
+              </Typography>
+
+              <Button
+                style={{
+                  backgroundColor: "#e00000",
+                  color: "#fff",
+                  margin: "10px 0 30px 0",
+                  padding: "8px 15px",
+                  borderRadius: "15px",
+                }}
+                onClick={async () => {
+                  const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+                  await firebase.auth().signInWithPopup(googleAuthProvider);
+                }}
+              >
+                <Typography variant="h6">Login Dengan Google</Typography>
+              </Button>
+            </div>
+          </Grid>
+          <Grid
+            item
+            lg={6}
+            md={12}
+            sm={12}
+            style={{ textAlign: "center", margin: "auto" }}
+          >
+            <img
+              alt=""
+              src={GodocArt}
+              width="500px"
+              style={{ marginBottom: "20px" }}
+            />
+          </Grid>
+        </Grid>
         {/* <Button
           style={{
             backgroundColor: "#e00000",
